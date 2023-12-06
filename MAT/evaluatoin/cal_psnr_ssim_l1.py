84d00204ab2d204b9aa1945e4a8dc2bec63d100c
jaisal-wispr <jkothari@stanford.edu>
2023-12-06 14:02:55 -0800

    Update cal_psnr_ssim_l1.py

--- MAT/evaluatoin/cal_psnr_ssim_l1.py
+++ MAT/evaluatoin/cal_psnr_ssim_l1.py
@@ -97,7 +97,7 @@ def calculate_metrics(folder1, folder2):
 
 
 if __name__ == '__main__':
-    folder1 = '../test_sets/Places/inpaint_results'
+    folder1 = '../test_sets_samples_4'
     folder2 = '../test_sets/Places/images'
 
     psnr, ssim, dl1 = calculate_metrics(folder1, folder2)
